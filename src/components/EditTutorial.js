1daca31b33b2182ec509a67b2615ae26e8385f6f
Rafael González <rafaelgonzalezllamas2001@gmail.com>
2024-07-06 16:48:25 +0200

    añadida edición de tutoriales y otros arreglos

--- src/components/EditTutorial.js
+++ src/components/EditTutorial.js
@@ -1,73 +1,186 @@
 import React, { useState, useEffect } from "react";
-import { Container, TextField, Button, Typography, Paper } from "@mui/material";
-import api from "../api";
+import api from "../api"; 
+import "../styles/AddTutorial.css";
+import {
+  Container,
+  TextField,
+  Button,
+  MenuItem,
+  Paper,
+  Box,
+  Typography,
+} from "@mui/material";
 import { useParams, useNavigate } from "react-router-dom";
+import "@mdxeditor/editor/style.css";
+import {
+  MDXEditor,
+  headingsPlugin,
+  listsPlugin,
+  quotePlugin,
+  UndoRedo,
+  toolbarPlugin,
+  BoldItalicUnderlineToggles,
+  linkPlugin,
+  linkDialogPlugin,
+  CreateLink,
+  InsertImage,
+  imagePlugin,
+  tablePlugin,
+  InsertTable,
+  markdownShortcutPlugin,
+  Separator,
+  BlockTypeSelect,
+  CodeToggle,
+} from "@mdxeditor/editor";
 
 const EditTutorial = () => {
   const { id } = useParams();
   const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [categories, setCategories] = useState([]);
   const [content, setContent] = useState("");
-  const [level, setLevel] = useState("");
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
   const baseURL = process.env.REACT_APP_API_URL;
 
   useEffect(() => {
     const fetchTutorial = async () => {
-      const res = await api.get(`${baseURL}/tutorials/${id}`);
-      setTitle(res.data.title);
-      setContent(res.data.content);
-      setLevel(res.data.level);
+      try {
+        const res = await api.get(`${baseURL}/tutorials/${id}`);
+        setTitle(res.data.title);
+        setContent(res.data.content);
+        setCategory(res.data.category);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching tutorial:", error);
+      }
     };
+
+    const fetchCategories = async () => {
+      try {
+        const response = await api.get(`${baseURL}/categories`);
+        setCategories(response.data);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+
     fetchTutorial();
-  }, [id]);
+    fetchCategories();
+  }, [id, baseURL]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     await api.patch(`${baseURL}/tutorials/${id}`, {
       title,
       content,
-      level,
+      category,
     });
     navigate("/tutorials");
   };
 
+  useEffect(() => {
+    if (title && category && content) {
+      setIsButtonDisabled(false);
+    } else {
+      setIsButtonDisabled(true);
+    }
+  }, [title, category, content]);
+
+  if (isLoading) {
+    return <Typography>Cargando...</Typography>;
+  }
+
   return (
-    <Container maxWidth="sm" style={{ marginTop: "2rem" }}>
-      <Paper elevation={3} style={{ padding: "1rem" }}>
-        <Typography variant="h4" gutterBottom>
-          Editar Tutorial
-        </Typography>
-        <form onSubmit={handleSubmit}>
+    <Container maxWidth="lg" className="add-tutorial-container">
+      <Paper elevation={3} className="add-tutorial-paper">
+        <Box display="flex" justifyContent="space-between" marginBottom={2}>
           <TextField
             label="Título"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
             fullWidth
             margin="normal"
+            style={{ marginRight: "1rem" }}
           />
           <TextField
-            label="Contenido"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+            select
+            label="Categoría"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
             fullWidth
             margin="normal"
-          />
-          <TextField
-            label="Nivel"
-            value={level}
-            onChange={(e) => setLevel(e.target.value)}
-            fullWidth
-            margin="normal"
-          />
-          <Button
-            variant="contained"
-            color="primary"
-            type="submit"
-            style={{ marginTop: "1rem" }}
+            style={{ marginLeft: "1rem" }}
           >
-            Guardar
-          </Button>
-        </form>
+            {categories.map((cat) => (
+              <MenuItem key={cat._id} value={cat._id}>
+                {cat.name}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Box>
+        <Box className="add-tutorial-editor">
+          <MDXEditor
+            markdown={content}
+            onChange={setContent}
+            plugins={[
+              tablePlugin(),
+              imagePlugin({
+                imageUploadHandler: async (file) => {
+                  const formData = new FormData();
+                  formData.append("image", file);
+
+                  try {
+                    const response = await api.post(`${baseURL}/upload`, formData, {
+                      headers: {
+                        "Content-Type": "multipart/form-data",
+                      },
+                    });
+
+                    const imageUrl = response.data.imageUrl;
+                    return imageUrl;
+                  } catch (error) {
+                    console.error("Error uploading image:", error);
+                    throw error;
+                  }
+                },
+              }),
+              linkPlugin(),
+              linkDialogPlugin(),
+              headingsPlugin(),
+              listsPlugin(),
+              linkPlugin(),
+              quotePlugin(),
+              markdownShortcutPlugin(),
+              toolbarPlugin({
+                toolbarContents: () => (
+                  <>
+                    <UndoRedo />
+                    <Separator />
+                    <BoldItalicUnderlineToggles />
+                    <BlockTypeSelect />
+                    <Separator />
+                    <CreateLink />
+                    <InsertTable />
+                    <InsertImage />
+                    <Separator />
+                    <CodeToggle />
+                  </>
+                ),
+              }),
+            ]}
+          />
+        </Box>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleSubmit}
+          className="add-tutorial-button"
+          disabled={isButtonDisabled}
+        >
+          Guardar
+        </Button>
       </Paper>
     </Container>
   );
