f99c6a9ebd673808b33742c4f4700808502f9c97
Rafael González <rafaelgonzalezllamas2001@gmail.com>
2025-07-03 23:36:14 +0200

    quitar pantalla de registro

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          {/* <Route path="/register" element={<Register />} /> */}
           <Route
             path="/exercises/:id"
             element={
